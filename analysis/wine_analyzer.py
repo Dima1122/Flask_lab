3de7a59fd7775a74464faa07f827a301d6ab2e49
Dima1122 <dm-avdeevyandex.ru>
2019-11-29 18:13:06 +0300

    test

--- analysis/wine_analyzer.py
+++ analysis/wine_analyzer.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import scipy.stats as st
 import sqlite3
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 def get_data(field1, field2):
     conn = sqlite3.connect(app.config['DATABASE'])
@@ -25,8 +27,56 @@ def get_coordinates(field1,  field2):
 
     return svg
 
+def standartize(data):
+    scaler = StandardScaler()
+    X = data.drop('Quality', axis = 1)
+    Y = data['Quality']
+    X = scaler.fit(X).transform(X)
+    return X,Y
+
+def get_important_components(components):
+    summ = 0
+    important_components = []
+    for component in components:
+        if summ >= 0.95:
+            return important_components, summ
+        summ += component[2]
+        important_components.append(component)
+    return important_components, summ
+
+def get_PCA(data):    
+    pca = PCA()
+    X, Y = standartize(data)
+    table_train = pca.fit_transform(X)
+    cov = pca.get_covariance()
+    [eigenvalues, eigenvectors] = np.linalg.eig(cov)
+    attr_and_info = []
+    for i in range(len(pca.explained_variance_ratio_)):
+        attr_and_info.append((data.drop('Quality', axis = 1).columns[i], eigenvalues[i], pca.explained_variance_ratio_[i]))
+    attr_and_info_sorted = sorted(attr_and_info, key=lambda x: x[2], reverse = True)
+    return get_important_components(attr_and_info_sorted)
+
 
 def calculate_pca(cols):
     data = get_all_data(cols)
-    #находим главные компоненты, объясняющие 95% дисперсии
+    components = get_PCA(data)
     return components
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
